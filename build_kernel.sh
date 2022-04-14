11c0275ae02de032f65ed01087c9b25d38c0155d
Leonardo Estacio <mdnssknght@gmail.com>
2022-04-14 16:55:10 -0500

    build_kernel.sh: support Clang/LLVM building
    references https://github.com/kdrag0n/proton-clang
    
    Signed-off-by: Leonardo Estacio <mdnssknght@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,5 +7,21 @@ export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
 
-make ARCH=arm64 a50_00_defconfig
-make ARCH=arm64 -j64
+# https://github.com/kdrag0n/proton-clang
+export PATH="$HOME/toolchains/proton-clang/bin:$PATH"
+
+make CC=clang CROSS_COMPILE=aarch64-linux-gnu- \
+    AR=llvm-ar \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    STRIP=llvm-strip \
+    a50_00_defconfig
+
+make CC=clang CROSS_COMPILE=aarch64-linux-gnu- \
+    AR=llvm-ar \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    STRIP=llvm-strip \
+    -j$(nproc --all)
